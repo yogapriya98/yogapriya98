86d297789464871a87116a0fa6c0b345340c8c35
yogapriya <yogapriya.s98@gmail.com>
2018-05-29 11:40:11 +0530

    basics of TypeConversion

---
+++ TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion{
+	public static void main(String s[]){
+	System.out.println("welcome to java");
+	byte one = 100;
+	byte two = 50;
+        int three = one + two;
+        System.out.println(three);
+}
+}
